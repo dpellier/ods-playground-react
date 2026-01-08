446da85b3e857e1956007c978ec04f308218dbb7
Damien Pellier <dpellier@gmail.com>
2026-01-08 11:46:45 +0100

    test

--- src/app/config.ts
+++
@@ -1,9 +0,0 @@
-const config = {
-  api: {
-    products: 'https://dummyjson.com/products',
-    signIn: 'https://dummyjson.com/auth/login',
-    users: 'https://dummyjson.com/users',
-  },
-}
-
-export { config }
--- src/app/constants/slice.ts
+++
@@ -1,8 +0,0 @@
-enum ACTION_STATUS {
-  failed = 'failed',
-  idle = 'idle',
-  pending = 'pending',
-  succeeded = 'succeeded',
-}
-
-export { ACTION_STATUS }
